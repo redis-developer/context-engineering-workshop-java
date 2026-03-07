99b00b6f791e9c614e67faec278809cd2287165a
Ricardo Ferreira <riferrei@riferrei.com>
2026-03-06 20:14:49 -0500

    Simplified with REST APIs

--- backend-layer/src/main/java/io/redis/devrel/workshop/controller/ChatController.java
+++ backend-layer/src/main/java/io/redis/devrel/workshop/controller/ChatController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 
 @RestController
 public class ChatController {
@@ -14,7 +13,7 @@ public class ChatController {
     private BasicChatAssistant assistant;
 
     @GetMapping("/ai/chat/string")
-    public Flux<String> chat(@RequestParam("query") String query) {
+    public String chat(@RequestParam("query") String query) {
         return assistant.chat(SYSTEM_PROMPT, query);
     }
 
